62757f76519674453288ec97e4f780ca919f0ceb
Delta.v <tmm.michalski@gmail.com>
2022-05-14 20:13:19 +0200

    chore: add basic router

--- frontend/blogapi/src/index.js
+++ frontend/blogapi/src/index.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+    Route,
+    BrowserRouter as Router,
+    Routes as Switch,
+} from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const routing = (
+    <Router>
+        <React.StrictMode>
+            <Header />
+            <Switch>
+                <Route exact path="/" component={App} />
+            </Switch>
+            <Footer />
+        </React.StrictMode>
+    </Router>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
